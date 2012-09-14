8bfc7bb53affde7dc0858d175dc0102ef1b59f02
0klein <0klein@rzpc162.informatik.uni-hamburg.de>
2012-09-14 11:30:25 +0200

    TextVerwalter hinzugefuegt.
    Signed-off-by: 0klein <0klein@rzpc162.informatik.uni-hamburg.de>,
    			   1jost <1jost@inf...>

---
+++ src/de/uni_hamburg/informatik/sep/zuul/TextVerwalter.java
@@ -0,0 +1,9 @@
+package de.uni_hamburg.informatik.sep.zuul;
+
+public class TextVerwalter
+{
+	public static final String EINLEITUNGSTEXT = "Nach einem fatalen Unfall in seinem Chemielabor ist der Wissenschaftler Dr. Little dazu verdammt in geringer Körpergröße eine Lösung seines Dilemmas zu finde. Er weiß, dass sein Kollege Prof. Dr. Evenbigger bei seinen Forschungen über das Verhalten von Sonnenblumen unter Aussetzung von Beta-Strahlung zufälligerweise auf ein Mittel gestoßen ist, mithilfe dessen Dr. Little seine normale Größe zurückerlangt. Prof. Dr.  Evenbiggers Labor befindet sich jedoch auf der anderen Seite des Universitätscampus und in Dr. Littles aktueller körperlicher Verfassung stellt die Reise eine große Herausforderung für ihn dar. Er muss sich nun mit seiner mäßigen Orientierung, er brauchte sich vor dem Unfall nicht sehr um das restliche Unigelände kümmern, das Labor des Professors finden. Ihnen obliegt die Verantwortung, Dr. Little sicher durch das Labyrinth von Räumen zu seinem Ziel zu bringen. Dabei besteht aber noch ein Problem: Dr. Little leidet stark unter  dem Einfluss seines fehlgeschlagenen Experiments und die körperliche Anstrengung des Durchreisen der Räume setzt so zu, dass er nach 15 Räumen ohne Nahrungsaufnahme den qualvollen Tod erwarten muss. Doch zum Glück haben unachtsame Studenten Kuchenstücke in verschiedenen Teilen des Campus  liegengelassen. Wenn Dr. Little es gelingt eines davon zu erwischen, so verbessert sich sein Gesundheitszustand, sodass er drei weitere Räume durchqueren kann. So starte nun in furchtloses Abenteuer und rette den Doktor vor seinem Verderben.";
+	public static final String SIEGTEXT = "Gute Arbeit. Sie haben Dr. Little erfolgreich an sein Ziel gebracht. Sein Kollege Prof. Dr. Evenbigger verabreicht ihm nun das Gegenmittel und verhilft ihm wieder zu seiner vollen Größe.";
+	public static final String NIEDERLAGETEXT = "";
+	public static final String BEENDENTEXT = "";
+}
