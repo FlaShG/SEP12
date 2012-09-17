3abf8554d082968921d21c3ebce7ecb6d4727032
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2012-09-17 13:42:54 +0200

    Program.main!

--- src/de/uni_hamburg/informatik/sep/zuul/Spiel.java
+++ src/de/uni_hamburg/informatik/sep/zuul/Spiel.java
@@ -58,30 +58,6 @@ public abstract class Spiel
 		_kontext.zeigeAusgaenge();
 	}
 
-	/**
-	 * main-Methode zum Ausführen.
-	 */
-	public static void main(String[] args)
-	{
-		boolean onconsole = args.length > 0 && args[0].equals("onconsole");
-		if(!onconsole)
-		{
-			SwingUtilities.invokeLater(new Runnable()
-			{
-				@Override
-				public void run()
-				{
-					Spiel spiel = new SpielGUI();
-					spiel.spielen();
-				}
-			});
-		}
-		else
-		{
-			//TODO: Console.
-		}
-
-	}
 
 	public abstract void schreibeNL(String nachricht);
 
