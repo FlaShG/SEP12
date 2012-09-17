df60686b0f48ced4bfb7e1e5b3e0643f34ea0d93
0schlund <0schlund@informatik.uni-hamburg.de>
2012-09-17 11:03:48 +0200

    Restschritte werden nicht mehr beim Siegtext angezeigt.

--- src/de/uni_hamburg/informatik/sep/zuul/SpielKontext.java
+++ src/de/uni_hamburg/informatik/sep/zuul/SpielKontext.java
@@ -3,13 +3,12 @@ package de.uni_hamburg.informatik.sep.zuul;
 import java.io.InputStream;
 import java.io.PrintStream;
 
-
 public class SpielKontext
 {
 	private static final int RAUMWECHSEL_ENERGIE_KOSTEN = 1;
 	private static final int KUCHEN_ENERGIE_GEWINN = 3;
 	private static final int START_ENERGIE = 8;
-	
+
 	private PrintStream _out;
 	private InputStream _in;
 
@@ -17,8 +16,6 @@ public class SpielKontext
 	private boolean _spielZuende;
 	private int _lebensEnergie;
 	private final Spiel _spiel;
-
-	
 
 	public SpielKontext(Spiel spiel)
 	{
@@ -30,23 +27,26 @@ public class SpielKontext
 	/**
 	 * Schreibt nachricht in den Output, hänge einen Zeilenumbruch an.
 	 * Vergleichbar mit PrintStream.println()
-	 * @param nachricht Die auszugebende Nachricht
+	 * 
+	 * @param nachricht
+	 *            Die auszugebende Nachricht
 	 */
 	public void schreibeNL(String nachricht)
 	{
 		_spiel.schreibeNL(nachricht);
 	}
-	
+
 	/**
-	 * Schreibt nachricht in den Output.
-	 * Vergleichbar mit PrintStream.print()
-	 * @param nachricht Die auszugebende Nachricht
+	 * Schreibt nachricht in den Output. Vergleichbar mit PrintStream.print()
+	 * 
+	 * @param nachricht
+	 *            Die auszugebende Nachricht
 	 */
 	public void schreibe(String nachricht)
 	{
 		_spiel.schreibe(nachricht);
 	}
-	
+
 	/**
 	 * Erzeugt alle Räume und verbindet ihre Ausgänge miteinander.
 	 */
@@ -58,6 +58,7 @@ public class SpielKontext
 
 	/**
 	 * Gibt den aktuellen Raum zurück, in dem sich der Spieler befindet.
+	 * 
 	 * @return
 	 */
 	public Raum getAktuellerRaum()
@@ -66,10 +67,12 @@ public class SpielKontext
 	}
 
 	/**
-	 * Ändert den aktuellen Raum, in dem sich der Spieler befindet.
-	 * Zeigt dessen Beschreibung an, welche Items eingesammelt werden
-	 * und zum Abschluss die Ausgänge.
-	 * @param aktuellerRaum der neue Raum, der betreten wird
+	 * Ändert den aktuellen Raum, in dem sich der Spieler befindet. Zeigt dessen
+	 * Beschreibung an, welche Items eingesammelt werden und zum Abschluss die
+	 * Ausgänge.
+	 * 
+	 * @param aktuellerRaum
+	 *            der neue Raum, der betreten wird
 	 */
 	public void setAktuellerRaum(Raum aktuellerRaum)
 	{
@@ -84,6 +87,7 @@ public class SpielKontext
 
 	/**
 	 * Gibt zurück, ob das Spiel zuende ist
+	 * 
 	 * @return true, wenn das Spiel zuende ist
 	 */
 	public boolean isSpielZuende()
@@ -91,18 +95,18 @@ public class SpielKontext
 		return _spielZuende;
 	}
 
-
 	/**
 	 * Gibt eine Nachricht aus und beendet das Spiel
-	 * @param nachricht die Nachricht, die vor dem Spielende ausgegeben werden soll
+	 * 
+	 * @param nachricht
+	 *            die Nachricht, die vor dem Spielende ausgegeben werden soll
 	 */
 	public void beendeSpiel(String nachricht)
 	{
 		schreibeNL(nachricht);
 		_spielZuende = true;
 	}
-	
-	
+
 	/**
 	 * Zeigt die Beschreibung des Raums an, in dem der Spieler sich momentan
 	 * befindet.
@@ -112,42 +116,45 @@ public class SpielKontext
 		schreibeNL(getAktuellerRaum().getBeschreibung());
 	}
 
-
 	/**
 	 * Zeigt die Ausgänge des aktuellen Raumes an.
 	 */
 	public void zeigeAusgaenge()
 	{
 		schreibe(TextVerwalter.AUSGAENGE + ": ");
-		
-		for(String s: getAktuellerRaum().getMoeglicheAusgaenge())
+
+		for(String s : getAktuellerRaum().getMoeglicheAusgaenge())
 		{
-			schreibe(s+" ");
+			schreibe(s + " ");
 		}
 
 		schreibeNL("");
 	}
-	
+
 	/**
-	 * Arbeitet alle Ereignisse ab, die beim Betrete eines Raumes auftreten können, wie
-	 * z.B. das Finden und Essen von Items.
+	 * Arbeitet alle Ereignisse ab, die beim Betrete eines Raumes auftreten
+	 * können, wie z.B. das Finden und Essen von Items.
 	 */
 	private void raumBetreten()
 	{
 		_lebensEnergie -= RAUMWECHSEL_ENERGIE_KOSTEN;
-		schreibeNL(TextVerwalter.RAUMWECHSELTEXT+_lebensEnergie);
-		switch(getAktuellerRaum().getItem())
+		switch (getAktuellerRaum().getItem())
 		{
-			case Kuchen:
-				_lebensEnergie += KUCHEN_ENERGIE_GEWINN;
-				schreibeNL(TextVerwalter.KUCHENGEFUNDENTEXT +_lebensEnergie);
+		case Keins:
+			schreibeNL(TextVerwalter.RAUMWECHSELTEXT + _lebensEnergie);			
 			break;
-			case Gegengift:
-				beendeSpiel(TextVerwalter.SIEGTEXT + "\n" + TextVerwalter.BEENDENTEXT);
+
+		case Kuchen:
+			_lebensEnergie += KUCHEN_ENERGIE_GEWINN;
+			schreibeNL(TextVerwalter.KUCHENGEFUNDENTEXT + _lebensEnergie);
+			break;
+		case Gegengift:
+			beendeSpiel(TextVerwalter.SIEGTEXT + "\n"
+					+ TextVerwalter.BEENDENTEXT);
 			break;
 		}
 		getAktuellerRaum().loescheItem();
-		
+
 		if(!isSpielZuende() && _lebensEnergie <= 0)
 		{
 			beendeSpiel(TextVerwalter.NIEDERLAGETEXT);
