34b91fe7d7c46bdb1b0c4498bbd5a846117988ec
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2012-09-17 15:14:14 +0200

    Some fixups

--- src/de/uni_hamburg/informatik/sep/zuul/SpielConsole.java
+++ src/de/uni_hamburg/informatik/sep/zuul/SpielConsole.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import de.uni_hamburg.informatik.sep.zuul.befehle.Befehl;
-
+import de.uni_hamburg.informatik.sep.zuul.spiel.SpielKontext;
 
 public class SpielConsole extends Spiel
 {
@@ -22,22 +22,42 @@ public class SpielConsole extends Spiel
 	{
 		System.out.println(nachricht);
 	}
-	
+
 	@Override
 	protected void spielen()
 	{
-		zeigeWillkommenstext();
+		_kontext = new SpielKontext(this);
 		
+		zeigeWillkommenstext();
+
 		while(!_kontext.isSpielZuende())
 		{
 			String eingabezeile = leseZeileEin();
-	        
-	        Befehl befehl = _parser.liefereBefehl(eingabezeile);
-	        
-	        befehl.ausfuehren(_kontext);
+
+			Befehl befehl = _parser.liefereBefehl(eingabezeile);
+
+			befehl.ausfuehren(_kontext);
 
 		}
 		
+		System.out
+				.println("Wollen Sie noch einmal spielen? Dann antworten Sie mit 'Ja'");
+		String zeile = leseZeileEin();
+		if(zeile.equals("Ja"))
+			restart();
+
+	}
+
+	protected void restart()
+	{
+		try
+		{
+			Runtime.getRuntime().exec("cls");
+		}
+		catch(IOException e)
+		{}
+
+		spielen();
 	}
 
 	/**
@@ -46,15 +66,18 @@ public class SpielConsole extends Spiel
 	String leseZeileEin()
 	{
 		System.out.print("> ");
-		
+
 		String eingabezeile = "";
 		BufferedReader eingabe = new BufferedReader(new InputStreamReader(
-		        System.in));
-		try {
-		    eingabezeile = eingabe.readLine();
-		} catch (IOException exc) {
-		    System.out.println("There was an error during reading: "
-		            + exc.getMessage());
+				System.in));
+		try
+		{
+			eingabezeile = eingabe.readLine();
+		}
+		catch(IOException exc)
+		{
+			System.out.println("There was an error during reading: "
+					+ exc.getMessage());
 		}
 		return eingabezeile;
 	}
