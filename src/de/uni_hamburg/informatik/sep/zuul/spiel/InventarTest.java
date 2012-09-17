5fc0ba926db2739b39850706932c992ba5e6807e
0schlund <0schlund@informatik.uni-hamburg.de>
2012-09-17 14:02:35 +0200

    Inventar Test auf Kuche erweitert.

--- src/de/uni_hamburg/informatik/sep/zuul/spiel/InventarTest.java
+++ src/de/uni_hamburg/informatik/sep/zuul/spiel/InventarTest.java
@@ -1,6 +1,9 @@
 package de.uni_hamburg.informatik.sep.zuul.spiel;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -51,6 +54,32 @@ public class InventarTest
 		
 		inv.fuegeItemHinzu(Item.Kuchen);
 		assertTrue(inv.isGefuellt());
+	}
+	
+	@Test
+	public void testHasAnyKuchen()
+	{
+		Inventar neuesInv = new Inventar();
+		assertFalse(neuesInv.hasAnyKuchen());
+
+		neuesInv.fuegeItemHinzu(Item.Kuchen);
+
+		assertTrue(neuesInv.hasAnyKuchen());
+	}
+
+	@Test
+	public void testGetAnyKuchen()
+	{
+		Inventar neuesInv = new Inventar();
+		assertFalse(neuesInv.hasAnyKuchen());
+
+		neuesInv.fuegeItemHinzu(Item.Kuchen);
+
+		assertTrue(neuesInv.hasAnyKuchen());
+
+		neuesInv.getAnyKuchen();
+
+		assertFalse(neuesInv.hasAnyKuchen());
 	}
 
 }
