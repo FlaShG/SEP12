329a0c823db7b6902ac6fe37e82c0990debaa6f4
1griese <1griese@rzpc166.informatik.uni-hamburg.de>
2012-09-18 13:52:21 +0200

    Anfang Raumstrukturparser + Raumsammlung.xml

--- src/de/uni_hamburg/informatik/sep/zuul/xml/RaumStrukturParser.java
+++ src/de/uni_hamburg/informatik/sep/zuul/xml/RaumStrukturParser.java
@@ -1,17 +1,24 @@
 package de.uni_hamburg.informatik.sep.zuul.xml;
 
+import java.util.List;
+
 public class RaumStrukturParser
 {
 	
 	private final String XMLPATH = "/xml_dateien/RaumSammlung.xml";
-	private RaumSammlung _root;
+//	private RaumSammlung _root;
 	
 	
 	public RaumStrukturParser()
 	{
 		
+		// TODO
 		
-		
+	}
+	
+	public List<XmlRaum> getXmlVerbindungen()
+	{
+		return null;
 	}
 	
 }
