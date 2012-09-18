e8510fc3930d7beaf70f9f7d5d362a2ca4fe478a
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2012-09-18 16:25:19 +0200

    .

--- tests/de/uni_hamburg/informatik/sep/zuul/befehle/BefehlEatTest.java
+++ tests/de/uni_hamburg/informatik/sep/zuul/befehle/BefehlEatTest.java
@@ -12,30 +12,30 @@ import de.uni_hamburg.informatik.sep.zuul.spiel.TextVerwalter;
 public class BefehlEatTest
 {
 
-	private BefehlEat befehlGo;
+	private BefehlEat befehlEat;
 
 	@Before
 	public void setUp() throws Exception
 	{
-		befehlGo = new BefehlEat();
-		befehlGo.setParameter(new String[] {"tasche"});
+		befehlEat = new BefehlEat();
+		befehlEat.setParameter(new String[] {"tasche"});
 	}
 
 	@Test
 	public void testAusfuehren()
 	{
-		befehlGo.setParameter(new String[] {"tasche"});
-		befehlGo.ausfuehren(new SpielKontext(new StubSchreiber()));
+		befehlEat.setParameter(new String[] {"tasche"});
+		befehlEat.ausfuehren(new SpielKontext(new StubSchreiber()));
 		
 
-		befehlGo.setParameter(new String[] {TextVerwalter.ORT_BODEN});
-		befehlGo.ausfuehren(new SpielKontext(new StubSchreiber()));
+		befehlEat.setParameter(new String[] {TextVerwalter.ORT_BODEN});
+		befehlEat.ausfuehren(new SpielKontext(new StubSchreiber()));
 	}
 
 	@Test
 	public void testGetBefehlsname()
 	{
-		befehlGo.getBefehlsname();
+		befehlEat.getBefehlsname();
 		
 		
 	}
