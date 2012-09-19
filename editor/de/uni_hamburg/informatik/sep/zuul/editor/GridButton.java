3382e8f5b9ddd3d23b95184ad1f884eae2917c18
1griese <1griese@rzpc172.informatik.uni-hamburg.de>
2012-09-19 11:35:12 +0200

    Gridbutton speichert koordniaten in raum.

--- editor/de/uni_hamburg/informatik/sep/zuul/editor/GridButton.java
+++ editor/de/uni_hamburg/informatik/sep/zuul/editor/GridButton.java
@@ -6,55 +6,55 @@ import java.awt.Dimension;
 import javax.swing.JButton;
 import de.uni_hamburg.informatik.sep.zuul.spiel.Raum;
 
-
 public class GridButton extends JButton
 {
 	private final int _x;
 	private final int _y;
 	private Raum _raum;
-	
+
 	public GridButton(int x, int y)
 	{
 		super();
 		_x = x;
 		_y = y;
-		
-		Dimension dim = new Dimension(30,30);
+
+		Dimension dim = new Dimension(30, 30);
 		setSize(dim);
 		setPreferredSize(dim);
 		setMinimumSize(dim);
 		setMaximumSize(dim);
-		
+
 		setAusgewaehlt(false);
 	}
-	
+
 	public int getGridX()
 	{
 		return _x;
 	}
-	
+
 	public int getGridY()
 	{
 		return _y;
 	}
-	
+
 	public void setAusgewaehlt(boolean b)
 	{
-		setBackground(b ?	(_raum != null ? new Color(0.6f, 0.6f, 0.9f) : Color.lightGray)
-						:	(_raum != null ? Color.blue : Color.gray));
+		setBackground(b ? (_raum != null ? new Color(0.6f, 0.6f, 0.9f)
+				: Color.lightGray) : (_raum != null ? Color.blue : Color.gray));
 	}
-	
+
 	public void fuegeRaumHinzu()
 	{
 		_raum = new Raum("", "");
 		setAusgewaehlt(true);
+		_raum.setKoordinaten(_x, _y);
 	}
-	
+
 	public Raum getRaum()
 	{
 		return _raum;
 	}
-	
+
 	public void loescheRaum()
 	{
 		_raum = null;
