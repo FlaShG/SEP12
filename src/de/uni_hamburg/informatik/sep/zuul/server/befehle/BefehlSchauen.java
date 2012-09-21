af2bad3398a3f49153afcbb3114bfc5b29819103
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2012-09-21 11:29:14 +0200

    Befehl Schauen

--- src/de/uni_hamburg/informatik/sep/zuul/server/befehle/BefehlSchauen.java
+++ src/de/uni_hamburg/informatik/sep/zuul/server/befehle/BefehlSchauen.java
@@ -1,105 +1,144 @@
 package de.uni_hamburg.informatik.sep.zuul.server.befehle;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 import de.uni_hamburg.informatik.sep.zuul.server.inventar.Item;
 import de.uni_hamburg.informatik.sep.zuul.server.raum.Raum;
 import de.uni_hamburg.informatik.sep.zuul.server.spiel.Spiel;
+import de.uni_hamburg.informatik.sep.zuul.server.spiel.Spieler;
+import de.uni_hamburg.informatik.sep.zuul.server.util.ServerKontext;
 import de.uni_hamburg.informatik.sep.zuul.server.util.TextVerwalter;
 
 public final class BefehlSchauen implements Befehl
 {
 
+	String[] _richtungen = new String[] { TextVerwalter.RICHTUNG_NORDEN,
+			TextVerwalter.RICHTUNG_OSTEN, TextVerwalter.RICHTUNG_SUEDEN,
+			TextVerwalter.RICHTUNG_WESTEN };
+
 	// TODO: broken
+
 	@Override
-	public void ausfuehren(SpielKontext kontext)
+	public boolean vorbedingungErfuellt(ServerKontext kontext, Spieler spieler,
+			Befehlszeile befehlszeile)
 	{
-		if(getParameters().length == 0)
-		{
-			Spiel.getInstance().schreibeNL(TextVerwalter.KEINESCHAURICHTUNG);
-			return;
-		}
-		String richtung = getParameters()[0];
-
-		Raum nebenRaum = kontext.getAktuellerRaum().getAusgang(richtung);
-
-		if(nebenRaum == null)
-		{
-
-		}
-		else
-		{
-			Spiel.getInstance().schreibeNL(
-					"Dr.Little schaut nach " + richtung + "en.");
-			Spiel.getInstance().schreibeNL("Er sieht: " + nebenRaum.getName());
-
-			Stack<Item> raumItems = (Stack<Item>) nebenRaum.getItems().clone();
-			Spiel.getInstance().schreibeNL(
-					"Zu sehen " + (raumItems.size() == 1 ? "ist" : "sind")
-							+ ":");
-
-			int anzahlKruemel = 0;
-			boolean gegengift = false;
-			boolean hatDinge = false;
-
-			if(raumItems.size() != 0)
-			{
-
-				while(raumItems.size() != 0)
-				{
-					if(raumItems.get(0) == Item.UKuchen
-							|| raumItems.get(0) == Item.UGiftkuchen
-							|| raumItems.get(0) == Item.IKuchen
-							|| raumItems.get(0) == Item.UKuchen)
-					{
-						anzahlKruemel++;
-					}
-					else if(raumItems.get(0) == Item.Gegengift)
-					{
-						gegengift = true;
-					}
-
-					raumItems.remove(0);
-				}
-
-			}
-			if(gegengift)
-			{
-				Spiel.getInstance().schreibeNL("Das Gegengift!");
-				hatDinge = true;
-			}
-
-			if(anzahlKruemel != 0)
-			{
-				Spiel.getInstance().schreibeNL(anzahlKruemel + " Krümel");
-				hatDinge = true;
-			}
-
-			if(nebenRaum.hasMaus())
-			{
-				Spiel.getInstance().schreibeNL("Eine Maus");
-				hatDinge = true;
-			}
-
-			//			if(nebenRaum.hasKatze())
-			//			{
-			//				Spiel.getInstance().schreibeNL("Eine Katze");
-			//				hatDinge = true;
-			//			}
-
-			if(!hatDinge)
-			{
-				Spiel.getInstance().schreibeNL("nur uninteressante Sachen");
-			}
-
-		}
-
+		String richtung = extrahiereRichtung(befehlszeile);
+		return befehlszeile.getGeparsteZeile().size() == 2
+				&& richtung != null && getRaumFuerRichtung(kontext, spieler, richtung) != null;
+	}
+	
+	/**
+	 * @param befehlszeile
+	 */
+	private String extrahiereRichtung(Befehlszeile befehlszeile)
+	{
+		String richtung = befehlszeile.getGeparsteZeile().get(1);
+		if(Arrays.asList(_richtungen).contains(richtung))
+			return richtung;
+		return null;
 	}
 
 	@Override
-	public String getBefehlsname()
+	public boolean ausfuehren(ServerKontext kontext, Spieler spieler,
+			Befehlszeile befehlszeile)
 	{
-		return TextVerwalter.BEFEHL_SCHAUEN;
+		String richtung = extrahiereRichtung(befehlszeile);
+		Raum nebenRaum = getRaumFuerRichtung(kontext, spieler, richtung);
+		
+		BefehlFactory.schreibeNL(kontext, spieler, 
+				"Dr.Little schaut nach " + richtung + "en.");
+		BefehlFactory.schreibeNL(kontext, spieler, "Er sieht: " + nebenRaum.getName());
+
+		Stack<Item> raumItems = (Stack<Item>) nebenRaum.getItems().clone();
+		BefehlFactory.schreibeNL(kontext, spieler, 
+				"Zu sehen " + (raumItems.size() == 1 ? "ist" : "sind") + ":");
+
+		int anzahlKruemel = 0;
+		boolean gegengift = false;
+		boolean hatDinge = false;
+
+		if(raumItems.size() != 0)
+		{
+
+			while(raumItems.size() != 0)
+			{
+				if(raumItems.get(0) == Item.UKuchen
+						|| raumItems.get(0) == Item.UGiftkuchen
+						|| raumItems.get(0) == Item.IKuchen
+						|| raumItems.get(0) == Item.UKuchen)
+				{
+					anzahlKruemel++;
+				}
+				else if(raumItems.get(0) == Item.Gegengift)
+				{
+					gegengift = true;
+				}
+
+				raumItems.remove(0);
+			}
+
+		}
+		if(gegengift)
+		{
+			BefehlFactory.schreibeNL(kontext, spieler, "Das Gegengift!");
+			hatDinge = true;
+		}
+
+		if(anzahlKruemel != 0)
+		{
+			BefehlFactory.schreibeNL(kontext, spieler, anzahlKruemel + " Krümel");
+			hatDinge = true;
+		}
+
+		if(nebenRaum.hasMaus())
+		{
+			BefehlFactory.schreibeNL(kontext, spieler, "Eine Maus");
+			hatDinge = true;
+		}
+
+		//			if(nebenRaum.hasKatze())
+		//			{
+		//				BefehlFactory.schreibeNL(kontext, spieler, "Eine Katze");
+		//				hatDinge = true;
+		//			}
+
+		if(!hatDinge)
+		{
+			BefehlFactory.schreibeNL(kontext, spieler, "nur uninteressante Sachen");
+		}
+		return false;
+	}
+
+	/**
+	 * @param kontext
+	 * @param spieler
+	 * @param richtung
+	 * @return
+	 */
+	private Raum getRaumFuerRichtung(ServerKontext kontext, Spieler spieler,
+			String richtung)
+	{
+		return kontext.getAktuellenRaumZu(spieler).getAusgang(richtung);
+	}
+
+	@Override
+	public void gibFehlerAus(ServerKontext kontext, Spieler spieler,
+			Befehlszeile befehlszeile)
+	{
+
+		if( befehlszeile.getGeparsteZeile().size() < 2)
+		{
+			BefehlFactory.schreibeNL(kontext, spieler, TextVerwalter.KEINESCHAURICHTUNG);
+		}
+		// TODO Keine gültige Richtung!
+		
+	}
+
+	@Override
+	public String[] getBefehlsnamen()
+	{
+		return new String[] { TextVerwalter.BEFEHL_SCHAUEN };
 	}
 
 }
