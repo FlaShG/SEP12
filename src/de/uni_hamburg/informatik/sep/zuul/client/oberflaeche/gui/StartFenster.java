43ddcff3014b29f5aa258a7b77b218132d1594c2
1griese <1griese@rzpc166.informatik.uni-hamburg.de>
2012-09-24 10:53:04 +0200

    Startfenster buttons implementiert

--- src/de/uni_hamburg/informatik/sep/zuul/client/oberflaeche/gui/StartFenster.java
+++ src/de/uni_hamburg/informatik/sep/zuul/client/oberflaeche/gui/StartFenster.java
@@ -4,11 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.net.MalformedURLException;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-
-import javax.swing.SwingWorker;
 
 import de.uni_hamburg.informatik.sep.zuul.client.ClientGUI;
 
@@ -24,10 +19,10 @@ public class StartFenster
 	public StartFenster()
 	{
 		_ui = new StartfensterUI();
-		
+
 		_ipAdresse = "127.0.0.1";
 		_port = 1090;
-		
+
 		initialisiereUI();
 	}
 
@@ -61,97 +56,107 @@ public class StartFenster
 			}
 		});
 
+		_ui.getBestaetigen().addActionListener(new ActionListener()
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				_spielername = _ui.getSpielerNameTextField().getText();
+				_ui.dispose();
+
+				try
+				{
+					new ClientGUI("RmiServer", _ipAdresse, _port, _spielername);
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+
+			}
+
+		});
+
+		_ui.getAbbrechenButton().addActionListener(new ActionListener()
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		});
+
 		_ui.getIPTextField().addKeyListener(new KeyListener()
 		{
 
 			@Override
 			public void keyTyped(KeyEvent arg0)
 			{
-				//  Auto-generated method stub
+				// TODO Auto-generated method stub
 
 			}
 
 			@Override
 			public void keyReleased(KeyEvent arg0)
 			{
-				// Auto-generated method stub
-
+				pruefeEingabe();
 			}
 
 			@Override
 			public void keyPressed(KeyEvent arg0)
 			{
-				if(arg0.getKeyCode() == KeyEvent.VK_ENTER)
-				{
-					pruefeIP();
-					pruefePort();
-					_spielername = _ui.getSpielerNameTextField().getText();
-					
-					try
-					{
-						new ClientGUI("RmiServer", _ipAdresse, _port, _spielername);
-					}
-					catch(Exception e)
-					{
-						e.printStackTrace();
-					}
+				// TODO Auto-generated method stub
 
-				}
-
-			}
-
-			private void pruefeIP()
-			{
-				SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>()
-				{
-
-					@Override
-					protected Void doInBackground() throws Exception
-					{
-						String[] tokens = _ui.getIPTextField().getText()
-								.split("\\.");
-						if(tokens.length == 4)
-						{
-							for(String str : tokens)
-							{
-								int i = Integer.parseInt(str);
-								if(!((i < 0) || (i > 255)))
-								{
-									_ipAdresse = _ui.getIPTextField().getText();
-								}
-							}
-						}
-						return null;
-					}
-				};
-
-				worker.execute();
-			}
-			
-			private void pruefePort()
-			{
-				SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>()
-				{
-
-					@Override
-					protected Void doInBackground() throws Exception
-					{
-						String eingabe = _ui.getPortTextField().getText();
-						
-						if (eingabe.matches("109[0-9]"))
-						{
-							_port = Integer.parseInt(eingabe);
-							System.out.println(eingabe);
-						}
-						
-						return null;
-					}
-				};
-
-				worker.execute();
 			}
 
 		});
+
+		_ui.getPortTextField().addKeyListener(new KeyListener()
+		{
+
+			@Override
+			public void keyTyped(KeyEvent e)
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e)
+			{
+				pruefeEingabe();
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 	}
 
+	private void pruefeEingabe()
+	{
+
+		String eingabeIP = _ui.getIPTextField().getText();
+		String eingabePort = _ui.getPortTextField().getText();
+
+		if(eingabeIP
+				.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")
+				&& eingabePort.matches("109[0-9]"))
+		{
+			_ui.getBestaetigen().setEnabled(true);
+			_ipAdresse = eingabeIP;
+			_port = Integer.parseInt(eingabePort);
+		}
+		else
+		{
+			_ui.getBestaetigen().setEnabled(false);
+		}
+
+	}
 }
