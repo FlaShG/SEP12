3a408f2e775df39ce446f3cc6633396063d12417
1roebe <1roebe@rzpc164.informatik.uni-hamburg.de>
2012-09-24 15:27:38 +0200

    Lebensenergiebalken implementiert

--- src/de/uni_hamburg/informatik/sep/zuul/client/ClientGUI.java
+++ src/de/uni_hamburg/informatik/sep/zuul/client/ClientGUI.java
@@ -146,6 +146,7 @@ public class ClientGUI extends Client
 	private void aktualisiereUI(ClientPaket paket, boolean vorschau)
 	{
 		aktualisiereMoeglicheAusgaenge(paket.getMoeglicheAusgaenge());
+		_bp.setLebensenergie(paket.getLebensEnergie());
 		
 		String nachricht = paket.getNachricht();
 		if(nachricht != null)
