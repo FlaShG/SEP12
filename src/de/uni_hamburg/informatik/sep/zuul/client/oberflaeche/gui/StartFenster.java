8a7986557c2b627cda1602d08cf683fffdeb5a46
0graeff <0graeff@informatik.uni-hamburg.de>
2012-09-24 09:52:08 +0200

    Multicatches entfernt.

--- src/de/uni_hamburg/informatik/sep/zuul/client/oberflaeche/gui/StartFenster.java
+++ src/de/uni_hamburg/informatik/sep/zuul/client/oberflaeche/gui/StartFenster.java
@@ -39,10 +39,8 @@ public class StartFenster
 				{
 					new ClientGUI("RmiServer", "127.0.0.1", 1090, "Dr. Little");
 				}
-				catch(MalformedURLException | RemoteException
-						| NotBoundException e1)
+				catch(Exception e1)
 				{
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				_ui.dispose();
@@ -88,10 +86,8 @@ public class StartFenster
 					{
 						new ClientGUI("RmiServer", _ipAdresse, 1090, _spielername);
 					}
-					catch(MalformedURLException | RemoteException
-							| NotBoundException e)
+					catch(Exception e)
 					{
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 
