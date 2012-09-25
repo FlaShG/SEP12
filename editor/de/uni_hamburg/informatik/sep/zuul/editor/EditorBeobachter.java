0d58d4234a9231db375ad2fdc7ab3f6a35128653
0graeff <0graeff@informatik.uni-hamburg.de>
2012-09-25 14:04:40 +0200

    Kommentare in EditorBeobachter.

--- editor/de/uni_hamburg/informatik/sep/zuul/editor/EditorBeobachter.java
+++ editor/de/uni_hamburg/informatik/sep/zuul/editor/EditorBeobachter.java
@@ -1,5 +1,12 @@
 package de.uni_hamburg.informatik.sep.zuul.editor;
 
+/**
+ * Beobachter-Interface für Editor-Ereignisse.
+ * Räume können ausgewählt (beinhaltet abwählen, erstellen und löschen),
+ * verändert oder verschoben werden.
+ * @author 0graeff
+ *
+ */
 public interface EditorBeobachter
 {
 	public void raumwahlUpdate();
