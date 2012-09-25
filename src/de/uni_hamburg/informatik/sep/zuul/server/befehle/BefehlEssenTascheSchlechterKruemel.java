3cffe432a86b533a7828ae6c0952016301e9b0c0
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2012-09-25 14:16:07 +0200

    removed redundant code + fancy shit a.s.o.

--- src/de/uni_hamburg/informatik/sep/zuul/server/befehle/BefehlEssenTascheSchlechterKruemel.java
+++ src/de/uni_hamburg/informatik/sep/zuul/server/befehle/BefehlEssenTascheSchlechterKruemel.java
@@ -1,61 +1,40 @@
 package de.uni_hamburg.informatik.sep.zuul.server.befehle;
 
 import de.uni_hamburg.informatik.sep.zuul.server.inventar.Item;
-import de.uni_hamburg.informatik.sep.zuul.server.spiel.SpielLogik;
 import de.uni_hamburg.informatik.sep.zuul.server.spiel.Spieler;
 import de.uni_hamburg.informatik.sep.zuul.server.util.ServerKontext;
 import de.uni_hamburg.informatik.sep.zuul.server.util.TextVerwalter;
 
-public class BefehlEssenTascheSchlechterKruemel implements Befehl
+class BefehlEssenTascheSchlechterKruemel extends BefehlEssenTasche
 {
-
-	private static final String BEFEHLSNAME = TextVerwalter.BEFEHL_ESSEN_TASCHE_SCHLECHT;
-	
 
 	@Override
 	public boolean vorbedingungErfuellt(ServerKontext kontext, Spieler spieler,
 			Befehlszeile befehlszeile)
 	{
-		return befehlszeile.getZeile().equals(BEFEHLSNAME)
-				&& spieler.getInventar().has(Item.IGiftkuchen);
+		return spieler.getInventar().has(Item.IGiftkuchen);
 	}
 
 	@Override
 	public boolean ausfuehren(ServerKontext kontext, Spieler spieler,
 			Befehlszeile befehlszeile)
 	{
-		esseSchlechterKruemel(kontext, spieler);
-		return true;
-	}
-
-	private void esseSchlechterKruemel(ServerKontext kontext, Spieler spieler)
-	{
-		if(spieler.getInventar().getKuchen(Item.IGiftkuchen) != null)
-		{
-			spieler.setLebensEnergie(spieler.getLebensEnergie()
-					- SpielLogik.GIFTKUCHEN_ENERGIE_VERLUST);
-			kontext.schreibeAnSpieler(spieler, TextVerwalter
-			.kuchengegessentext(spieler.getLebensEnergie()));
-		}
+		Item kuchen = spieler.getInventar().getKuchen(Item.IGiftkuchen);
+		return esseKuchen(kontext, spieler, kuchen);
 	}
 
 	@Override
 	public void gibFehlerAus(ServerKontext kontext, Spieler spieler,
 			Befehlszeile befehlszeile)
 	{
-		kontext.schreibeAnSpieler(spieler, TextVerwalter.KEINIDENTIFIZIERTERKUCHEN);
+		kontext.schreibeAnSpieler(spieler,
+				TextVerwalter.KEINIDENTIFIZIERTERKUCHEN);
 	}
 
 	@Override
 	public String[] getBefehlsnamen()
 	{
-		return new String[] { BEFEHLSNAME };
-	}
-
-	@Override
-	public String getHilfe()
-	{
-		return TextVerwalter.HILFE_EAT;
+		return new String[] { TextVerwalter.BEFEHL_ESSEN_TASCHE_SCHLECHT };
 	}
 
 }
--- src/de/uni_hamburg/informatik/sep/zuul/server/befehle/BefehlFuettereUnbekanntenKruemel.java
+++ src/de/uni_hamburg/informatik/sep/zuul/server/befehle/BefehlFuettereUnbekanntenKruemel.java
@@ -1,146 +1,39 @@
 package de.uni_hamburg.informatik.sep.zuul.server.befehle;
 
-import java.util.LinkedList;
-
-import de.uni_hamburg.informatik.sep.zuul.server.features.Katze;
 import de.uni_hamburg.informatik.sep.zuul.server.inventar.Item;
-import de.uni_hamburg.informatik.sep.zuul.server.raum.Raum;
 import de.uni_hamburg.informatik.sep.zuul.server.spiel.Spieler;
-import de.uni_hamburg.informatik.sep.zuul.server.util.FancyFunction;
 import de.uni_hamburg.informatik.sep.zuul.server.util.ServerKontext;
 import de.uni_hamburg.informatik.sep.zuul.server.util.TextVerwalter;
 
-public class BefehlFuettereUnbekanntenKruemel implements Befehl
+class BefehlFuettereUnbekanntenKruemel extends BefehlFuettere
 {
-
-	public static final String BEFEHLSNAME = TextVerwalter.BEFEHL_FUETTERE
-			+ " " + "maus";
-
-	/**
-	 * Bestimmt die Richtung, die die Maus empfiehlt abhängig davon, ob der
-	 * Kuchen giftig ist.
-	 */
-	static String bestimmeRichtung(Item kuchen, String richtigeRichtung,
-			String[] moeglicheRichtungen)
-	{
-		if(kuchen == Item.UKuchen || kuchen == Item.IKuchen)
-		{
-			return richtigeRichtung;
-		}
-		if(kuchen == Item.UGiftkuchen || kuchen == Item.IGiftkuchen)
-		{
-			LinkedList<String> richtungen = new LinkedList<String>();
-
-			for(String richtung : moeglicheRichtungen)
-				richtungen.add(richtung);
-
-			richtungen.remove(richtigeRichtung);
-
-			String falscheRichtung = FancyFunction.getRandomEntry(richtungen);
-
-			// Falls der Raum nur einen Ausgang hat.
-			if(falscheRichtung == null)
-				falscheRichtung = richtigeRichtung;
-
-			return falscheRichtung;
-		}
-
-		return null;
-	}
 
 	@Override
 	public boolean vorbedingungErfuellt(ServerKontext kontext, Spieler spieler,
 			Befehlszeile befehlszeile)
 	{
-		// Wenn eine Katze oder eine Maus gefüttert werden könnte
-		Raum raum = kontext.getAktuellenRaumZu(spieler);
-		return (raum.hasKatze() || raum.hasMaus())
-				&& (spieler.getInventar().has(Item.UKuchen) || spieler
-						.getInventar().has(Item.UGiftkuchen));
+		return super.vorbedingungErfuellt(kontext, spieler, befehlszeile)
+				&& spieler.getInventar().hasAnyUKuchen();
 	}
 
 	@Override
 	public boolean ausfuehren(ServerKontext kontext, Spieler spieler,
 			Befehlszeile befehlszeile)
 	{
-		{
-			// Versuche eine Katze oder eine Maus zu füttern
-
-			Raum raum = kontext.getAktuellenRaumZu(spieler);
-
-			//wenn katze im raum
-
-			if(raum.hasKatze())
-			{
-
-				Katze katze = raum.getKatze();
-
-				if(katze.isSatt())
-				{
-					kontext.schreibeAnSpieler(spieler,
-							TextVerwalter.KATZE_HAT_KEINEN_HUNGER);
-					return false;
-				}
-				Item kuchen = spieler.getInventar().getAnyUKuchen();
-				katze.fuettere(kontext, spieler, kuchen);
-
-				return true;
-			}
-			else if(raum.hasMaus())
-			{
-				Item kuchen = null;
-
-				if(spieler.getInventar().has(Item.UKuchen)
-						&& spieler.getInventar().has(Item.UGiftkuchen))
-				{
-					kuchen = spieler.getInventar().getAnyUKuchen();
-				}
-				else if(spieler.getInventar().has(Item.UKuchen))
-				{
-					kuchen = spieler.getInventar().getKuchen(Item.UKuchen);
-				}
-				else
-				{
-					kuchen = spieler.getInventar().getKuchen(Item.UGiftkuchen);
-				}
-
-				Raum aktuellerRaum = kontext.getAktuellenRaumZu(spieler);
-
-				String richtigeRichtung = aktuellerRaum.getMaus().getRichtung();
-
-				String[] moeglicheRichtungen = aktuellerRaum
-						.getMoeglicheAusgaenge();
-
-				String richtung = bestimmeRichtung(kuchen, richtigeRichtung,
-						moeglicheRichtungen);
-
-				String richtungsangabe = String.format(
-						TextVerwalter.MAUS_RICHTUNGSANGABE, richtung);
-
-				kontext.schreibeAnSpieler(spieler, richtungsangabe);
-
-				return true;
-			}
-			return false;
-		}
+		Item kuchen = spieler.getInventar().getAnyUKuchen();
+		return fuettereTierMit(kontext, spieler, kuchen);
 	}
 
 	@Override
 	public void gibFehlerAus(ServerKontext kontext, Spieler spieler,
 			Befehlszeile befehlszeile)
 	{
-		Raum raum = kontext.getAktuellenRaumZu(spieler);
-
-		if(!spieler.getInventar().has(Item.UKuchen)
-				|| !spieler.getInventar().has(Item.UGiftkuchen))
+		if(!spieler.getInventar().hasAnyUKuchen())
 		{
 			kontext.schreibeAnSpieler(spieler, TextVerwalter.MAUS_KEIN_KRUEMEL);
+			return;
 		}
-		else if(!raum.hasKatze() && !raum.hasMaus())
-		{
-			kontext.schreibeAnSpieler(spieler,
-					TextVerwalter.BEFEHL_FUETTERE_NICHTS_DA_ZUM_FUETTERN);
-		}
+		super.gibFehlerAus(kontext, spieler, befehlszeile);
 	}
 
 	@Override
@@ -148,11 +41,4 @@ public class BefehlFuettereUnbekanntenKruemel implements Befehl
 	{
 		return new String[] { TextVerwalter.BEFEHL_FUETTERE_UNBEKANNT };
 	}
-
-	@Override
-	public String getHilfe()
-	{
-		return TextVerwalter.HILFE_FEED;
-	}
-
 }
