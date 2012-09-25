c41330978be300dde0986f848a882e16fa51cae7
0graeff <0graeff@informatik.uni-hamburg.de>
2012-09-25 15:12:52 +0200

    EigenschaftIntPanelGT1 hinzugefügt.

---
+++ editor/de/uni_hamburg/informatik/sep/zuul/editor/EigenschaftIntPanelGT1.java
@@ -0,0 +1,37 @@
+package de.uni_hamburg.informatik.sep.zuul.editor;
+
+import javax.swing.SpinnerNumberModel;
+
+/**
+ * Ein Panel, das einen mit JLabel beschrifteten JSpinner anzeigt.
+ * Der Wert kann niemals kleiner als 1 werden.
+ * 
+ * @author 0graeff
+ */
+public class EigenschaftIntPanelGT1 extends EigenschaftIntPanel
+{
+	/**
+	 * Erzeugt ein neues {@link EigenschaftIntPanel} mit Startwert.
+	 * 
+	 * @param beschriftung der Text des davorstehenden Labels
+	 * @param startwert der Wert, den die Komponente annimmt
+	 */
+	public EigenschaftIntPanelGT1(String beschriftung, int startwert)
+	{
+		this(beschriftung, startwert, null);
+	}
+	
+	/**
+	 * Erzeugt ein Eigenschaftsfeld mit einem Default-wert und Observer
+	 * 
+	 * @param beschriftung der Text des davorstehenden Labels
+	 * @param startwert der Wert, den die Komponente annimmt
+	 * @param beobachter der {@link EditorBeobachter}, der über Änderungen im Eingabefeld informiert werden soll
+	 */
+	public EigenschaftIntPanelGT1(String beschriftung, int startwert,
+			EditorBeobachter beobachter)
+	{
+		super(beschriftung, startwert, beobachter);
+		_zahl.setModel(new SpinnerNumberModel(startwert, 1, 99, 1));
+	}
+}
