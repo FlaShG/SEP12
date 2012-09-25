052bd72cdaabe93dc096b98bfb835036cd3d0ec3
1jost <1jost@rzpc164.informatik.uni-hamburg.de>
2012-09-25 17:13:15 +0200

    Client-Klassenkommentar 'gekürzt'
    Signed-off-by: 1jost <1jost@rzpc164.informatik.uni-hamburg.de>

--- src/de/uni_hamburg/informatik/sep/zuul/client/Client.java
+++ src/de/uni_hamburg/informatik/sep/zuul/client/Client.java
@@ -9,20 +9,7 @@ import java.rmi.server.UnicastRemoteObject;
 import de.uni_hamburg.informatik.sep.zuul.server.ServerInterface;
 
 /**
- * Dies ist die Hauptklasse der Anwendung "Die Welt von Zuul". "Die Welt von
- * Zuul" ist ein sehr einfaches, textbasiertes Adventure-Game. Ein Spieler kann
- * sich in einer Umgebung bewegen, mehr nicht. Das Spiel sollte auf jeden Fall
- * ausgebaut werden, damit es interessanter wird!
- * 
- * Zum Spielen muss eine Instanz dieser Klasse erzeugt werden und an ihr die
- * Methode "spielen" aufgerufen werden.
- * 
- * Diese Instanz erzeugt und initialisiert alle anderen Objekte der Anwendung:
- * Sie legt alle Räume und einen Parser an und startet das Spiel. Sie wertet
- * auch die Befehle aus, die der Parser liefert, und sorgt für ihre Ausführung.
- * 
- * Das Ausgangssystem basiert auf einem Beispielprojekt aus dem Buch
- * "Java lernen mit BlueJ" von D. J. Barnes und M. Kölling.
+ * Abstrakte Client-Klasse.
  */
 public abstract class Client extends UnicastRemoteObject implements
 		ClientInterface
