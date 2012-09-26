41460202e234dcbb4ecfdb30ab29cc96fbf30b3d
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2012-09-26 09:58:51 +0100

    FancyFunction test. noch kein test f�r innere klasse.

---
+++ tests/de/uni_hamburg/informatik/sep/zuul/server/util/FancyFunctionTest.java
@@ -0,0 +1,48 @@
+package de.uni_hamburg.informatik.sep.zuul.server.util;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FancyFunctionTest {
+	
+	private List<Object> _linkedListe;
+	private List<Object> _arrayListe;
+	private Object o;
+	
+	@Before
+	public void setUp()
+	{
+		_linkedListe = new LinkedList<>();
+		_arrayListe = new ArrayList<>();
+		o = new Object();
+		_linkedListe.add(o);
+		_arrayListe.add(o);
+	}
+
+	@Test
+	public void testGetRandomEntry() {
+		Object entry = FancyFunction.getRandomEntry(_linkedListe);
+		
+		assertEquals(o, entry);
+		
+		entry = FancyFunction.getRandomEntry(_arrayListe);
+		
+		assertEquals(o, entry);
+		
+		_linkedListe.remove(0); //leereListe
+		 entry = FancyFunction.getRandomEntry(_linkedListe);
+		 assertNull(entry);
+		 
+		 String s = "bla";
+		 _arrayListe.add(s);
+		 
+		 assertNotNull(FancyFunction.getRandomEntry(_arrayListe));
+	}
+
+}
