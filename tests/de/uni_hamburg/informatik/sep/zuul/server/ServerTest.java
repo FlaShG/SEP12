71049980c7725b3c38a7b88ec1fab2283540c018
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2012-09-27 15:49:28 +0100

    vollstaendiger server test.

--- tests/de/uni_hamburg/informatik/sep/zuul/server/ServerTest.java
+++ tests/de/uni_hamburg/informatik/sep/zuul/server/ServerTest.java
@@ -1,21 +1,24 @@
 package de.uni_hamburg.informatik.sep.zuul.server;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
-import java.rmi.NotBoundException;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.sun.security.ntlm.Client;
-
 import de.uni_hamburg.informatik.sep.zuul.client.ClientInterface;
+import de.uni_hamburg.informatik.sep.zuul.client.ClientPaket;
 
 public class ServerTest {
 
@@ -96,31 +99,67 @@ public class ServerTest {
 
 		_server.loginClient(c1, "c1");
 		_server.loginClient(c2, "c2");
-		
-		assertTrue(_server.empfangeNutzerEingabe("eingabe", "c1"));
-		verify(_server, atLeastOnce()).sendeAenderungen(anyList());
-		
-		
+
+		assertTrue(_server.empfangeNutzerEingabe("gehe nord", "c1"));
+		assertTrue(_server.empfangeNutzerEingabe("eingabe", "c2"));
+		verify(c1, atLeastOnce()).zeigeAn(any(ClientPaket.class));
+		verify(c2, atLeastOnce()).zeigeAn(any(ClientPaket.class));
 
 		_server.beendeServer();
 	}
 
 	@Test
-	public void testEmpfangeStartEingabe() {
+	public void testEmpfangeStartEingabe() throws RemoteException {
+		// nur ein spieler loggt ein und startet:
+		ClientInterface c1 = mock(ClientInterface.class);
+
+		assertTrue(_server.getConnectedClients().isEmpty());
+
+		_server.loginClient(c1, "c1");
+		_server.empfangeStartEingabe("c1");
+
+		verify(c1, atLeastOnce()).zeigeAn(any(ClientPaket.class));
+
 		_server.beendeServer();
 	}
 
 	@Test
-	public void testGetConnectedClients() {
+	public void testGetConnectedClients() throws RemoteException {
+
+		// einloggen
+		ClientInterface c1 = mock(ClientInterface.class);
+		ClientInterface c2 = mock(ClientInterface.class);
+
+		assertTrue(_server.getConnectedClients().isEmpty());
+
+		_server.loginClient(c1, "c1");
+		_server.loginClient(c2, "c2");
+
+		assertTrue(_server.getConnectedClients().values().contains(c1));
+		assertTrue(_server.getConnectedClients().values().contains(c2));
+
 		_server.beendeServer();
 	}
 
 	@Test
-	public void testUpdate() {
-		_server.beendeServer();
-	}
+	public void testUpdate() throws RemoteException {
+		// einloggen
+		ClientInterface c1 = mock(ClientInterface.class);
+		ClientInterface c2 = mock(ClientInterface.class);
 
-	protected void tearDown() {
+		assertTrue(_server.getConnectedClients().isEmpty());
+
+		_server.loginClient(c1, "c1");
+		_server.loginClient(c2, "c2");
+		
+		_server.update(null, null);
+		verify(c1, atLeastOnce()).zeigeAn(any(ClientPaket.class));
+		verify(c2, atLeastOnce()).zeigeAn(any(ClientPaket.class));
+		
+		String[] array = {"c1", "nord"};
+		_server.update(null, array);
+		verify(c1, atLeastOnce()).zeigeVorschau(any(ClientPaket.class));
+
 		_server.beendeServer();
 	}
 
